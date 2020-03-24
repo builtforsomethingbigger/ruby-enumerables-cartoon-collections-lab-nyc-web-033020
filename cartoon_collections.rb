3a9d9fb0a71cd1e0b53ebe49e590d681cefae68c
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-24 00:19:30 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,10 @@
 def roll_call_dwarves(array)
-  # Your code here
   array.each_with_index do |dwarf, index|
     puts "#{index + 1}. #{dwarf}"
 end
 
-end
-
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array = ["Earth", "Wind", "Fire", "Water", "Heart"]
 end
 
 def long_planeteer_calls# code an argument here
