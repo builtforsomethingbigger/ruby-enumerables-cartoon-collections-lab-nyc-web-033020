22bd884cb44917a1afa0f354cfc1d72cac9594d0
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-24 00:20:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,11 +3,12 @@ def roll_call_dwarves(array)
     puts "#{index + 1}. #{dwarf}"
 end
 
-def summon_captain_planet(["Earth", "Wind", "Fire", "Water", "Heart"])
+def summon_captain_planet(array)
+  array = ["Earth", "Wind", "Fire", "Water", "Heart"]
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(summon)
+  summon.any? { |i| i.length > 4 }
 end
 
 def find_the_cheese# code an argument here
