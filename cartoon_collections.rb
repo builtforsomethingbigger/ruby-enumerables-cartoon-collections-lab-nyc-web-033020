699572fb5688720775f106f6ade4a68e00b148ca
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-24 00:26:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,8 +9,10 @@ def summon_captain_planet(summon)
   summon.map { |planeteer| planeteer.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(summon)
+  summon.any? do |call|
+  call.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
